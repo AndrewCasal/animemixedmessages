b85b1e7e861a7407b54b8848eab75dedf1eb96d5
andrew <casal.andrew124@gmail.com>
2021-11-13 18:28:08 -0500

    made function for message, need to fix random #

--- animecode.js
+++ animecode.js
@@ -5,4 +5,9 @@ const randNumber = Math.floor(Math.random() * 10);
 const randChar = char[randNumber];
 const randItem = item[randNumber];
 const randPower = power[randNumber];
-console.log(randPower);
+const ultimateAnimeWarrior = () => {
+    console.log(`Your ultimate anime warrier is ${randChar} with the use of ${randItem} and the ability to ${randPower}!`);
+}
+ultimateAnimeWarrior();
+//Currently is only doing the randNumber once so character, item, and power are all in the same index.
+//Need to rewrite code so that a random number is run every time variable is called. Maybe let instead of const?
