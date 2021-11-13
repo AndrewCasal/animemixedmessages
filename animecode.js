15bde3190a7cf261873a6eebf0c102b3261adabe
andrew <casal.andrew124@gmail.com>
2021-11-13 17:48:29 -0500

    array for items

--- animecode.js
+++ animecode.js
@@ -1,1 +1,2 @@
 const char = ['Goku', 'Luffy', 'Naruto', 'Ichigo', 'Eren Jaeger', 'Deku', 'Yusuke', 'Saitama', 'Jotaro Kujo', 'Meliodas'];
+const item = ['Dragon Balls', 'Devil Fruit', 'Kunai', 'Substitute Shinigami Badge', 'Omni-Directional Mobility Gear', 'Grenade Bracer', 'Spirit Sword', "Child Emporer's Backpack", 'Star Platinum', 'Spirit Spear Chastiefoil' ];
